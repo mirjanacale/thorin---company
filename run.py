cf54c66bce924ba1b714b6941dd1d6429def0b0d
Mirjana Cale <mirjana083@gmail.com>
2024-09-16 22:15:42 +0000

    Add careers.html

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
      return render_template("index.html")
  
+ 
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -16,11 +17,12 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html")#
 
- 
 
-    
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
